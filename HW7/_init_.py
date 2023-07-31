4f35da09bc33ea202be2698f9fe8aa5b274d0dc0
Rav53 <Rav5385@gmail.com>
2023-07-31 20:23:58 +0300

    HW6-7PythonTest

---
+++ HW7/_init_.py
@@ -0,0 +1,7 @@
+__all__ = ["fill_numbers",
+           "files_splitting",
+           "group_renaming",
+           "create_files",
+           "name_generator",
+           "create_files_2",
+           "new_dir_creating"]
---
+++ HW7/new_dir_creating.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import os
+
+
+def new_dir_creating(name_dir: str):
+    name = Path(Path.cwd() / name_dir)
+    if not name.exists():       # проверка наличия дир-ии
+        name.mkdir()            # создание директории
+    os.chdir(name)              # переход в созданный каталог, делая его текущим
